322898d9855f8e6f507b4f58c41468941f86062e
ani2340 <121184004+ani2340@users.noreply.github.com>
2023-02-23 13:36:40 +0000

    class quiz assignment

--- card.py
+++ card.py
@@ -1,6 +1,16 @@
-# Name:
+# Name:Anisha Oyola
 
 class Card:
     """class representing a simple Neapolitan card. Holds a 
     value and a suit"""
-    
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+    def __repr__(self):
+        return f'Card(value = {self.value}, suit = {self.suit})'
+    def __str__(self):
+        return f'{self.value} of {self.suit}'
+    def __eq__(self, other):
+        self.other = other
+        if self.value == self.other:
+            return True
